3c810bbffd5f99f0a2f1ab338033d9f41d7a29b1
zp622 <848867052@qq.com>
2019-03-20 16:44:10 +0800

    commit

---
+++ src/views/CommonComponents/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="layout">
+      this is layout.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/CommonComponents/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    this is login
+    <el-button>跳转</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
