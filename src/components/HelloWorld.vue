3f0763cc3447cb707aed0f3dc789105a9de60e99
zp622 <848867052@qq.com>
2019-03-20 13:33:39 +0800

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,22 +5,6 @@
     <ul>
       <li>
         <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
           href="https://chat.vuejs.org"
           target="_blank"
         >
